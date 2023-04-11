3d53b82591d1ba5d660a7387e78c382edde9a976
zzanggeonui <120348599+zzanggeonui@users.noreply.github.com>
2023-04-11 16:11:54 +0900

    Update Config.java

--- app/src/main/java/com/rjsgml1105/googlemapapi/config/Config.java
+++ app/src/main/java/com/rjsgml1105/googlemapapi/config/Config.java
@@ -7,7 +7,7 @@ public class Config {
     public static final String DOMAIN_TEST = "http://localhost:5000";
 
 
-    public  static final String MAPS_API_KEY = "AIzaSyA6B2_uj9sHqY_tlX7trbzkKE6iE8hqgk8";
+    public  static final String MAPS_API_KEY = "google-key";
     public static final String PREFERENCE_NAME = "GoogleMapApi";
     public static final String ACCESS_TOKEN = "access_token";
 
